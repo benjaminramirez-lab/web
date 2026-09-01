399ff4f71d2cc5f1632712745a50fbb164d3343f
benjaminramirez-lab <benjamin.ramirez@puentemaipo.cl>
2026-09-01 11:27:02 -0400

    web

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
-//inp init -y
-//Prender server es con node app.js
-//npm run dev <= modo de desarrollo 
-//
-
-//PASO 1
 const express = require('express');
-const path = require('path');
 
-//PASO 2
 const app = express();
 
-//PASO 3 - Crear puerto de conexion servidor
 const PORT = 3000;
 
-//PASO 4 - Levatar motor de plantilla
+
+//configurar ejs
 app.set('view engine', 'ejs');
 
-//PASO 5 - Archivos estaticos
-app.use(express.static(path.join(__dirname,'public')));
-
 //RUTAS
-//Index(Home)
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-//Servicios
 app.get('/servicios', (req, res) => {
     res.render('servicios');
 });
 
-
-//levantar servido
-app.listen(PORT, () => {
-    console.log("Servidor Arriba");
+app.get('/nosotros', (req, res) => {
+    res.render('nosotros');
 });
+
+app.get('/contactos', (req, res) => {
+    res.render('contactos');
+});
+
+app.listen(PORT, () =>{
+    console.log('Servidor corriendo em http://localhost:&(PORT)');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//inp init -y
+//Prender server es con node app.js
+//npm run dev <= modo de desarrollo 
+//$ npm install ejs <= italador de ejs
