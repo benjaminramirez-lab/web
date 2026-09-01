57c99a14a9cac67518fe8bd2e306c90b6d4e2ba4
benjaminramirez-lab <benjamin.ramirez@puentemaipo.cl>
2026-09-01 10:48:18 -0400

    base proyecto

---
+++ app.js
@@ -0,0 +1,34 @@
+//inp init -y
+//Prender server es con node app.js
+//PASO 1
+const express = require('express');
+const path = require('path');
+
+//PASO 2
+const app = express();
+
+//PASO 3 - Crear puerto de conexion servidor
+const PORT = 3000;
+
+//PASO 4 - Levatar motor de plantilla
+app.set('view engine', 'ejs');
+
+//PASO 5 - Archivos estaticos
+app.use(express.static(path.join(__dirname,'public')));
+
+//RUTAS
+//Index(Home)
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//Servicios
+app.get('/servicios', (req, res) => {
+    res.render('servicios');
+});
+
+
+//levantar servido
+app.listen(PORT, () => {
+    console.log("Servidor Arriba");
+});
